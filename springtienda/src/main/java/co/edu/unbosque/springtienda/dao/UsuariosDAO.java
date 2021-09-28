eff815a74f8f0a073d2218042088430bfbf207b6
cristian1767 <90486186+cristian1767@users.noreply.github.com>
2021-09-27 21:32:08 -0500

    Delete springtienda directory

--- springtienda/src/main/java/co/edu/unbosque/springtienda/dao/UsuariosDAO.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.springtienda.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.unbosque.springtienda.model.Usuarios;
-
-public interface UsuariosDAO extends JpaRepository<Usuarios, Integer> {
-
-}
